d1f4a811d7681a6c39a54469b3e6d259860f5624
unknown <tcutlip08@gmail.com>
2019-07-06 09:34:28 -0400

    function loads properly. changed to .on(..., 'button')

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -31,34 +31,36 @@ $("#addGif").on("click", function (event) {
     $("#gifInput").val("");
 });
 
-$("button").on("click", function () {
-    console.log("dude");
+// <button class="btn btn-secondary gif" data-name="cat">cat</button>
+// <button class="btn btn-secondary gif" data-name="cat">cat</button>
+$(document).on("click", "button", function () {
     var gif = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        gif + "&api_key=TI0kxI9vVsmT8MAq2r6BCEoVozy5t7xz";
+    console.log(gif);
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    //     gif + "&api_key=TI0kxI9vVsmT8MAq2r6BCEoVozy5t7xz&limit=1";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response);
-            var results = response.data;
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // })
+    //     .then(function (response) {
+    //         console.log(response);
+    //         var results = response.data;
 
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
+    //         for (var i = 0; i < results.length; i++) {
+    //             var gifDiv = $("<div>");
 
-                var rating = results[i].rating;
+    //             var rating = results[i].rating;
 
-                var p = $("<p>").text("Rating: " + rating);
+    //             var p = $("<p>").text("Rating: " + rating);
 
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height.url);
+    //             var gifImage = $("<img>");
+    //             gifImage.attr("src", results[i].images.fixed_height.url);
 
-                gifDiv.prepend(p);
-                gifDiv.prepend(gifImage);
+    //             gifDiv.prepend(p);
+    //             gifDiv.prepend(gifImage);
 
-                $("#displayGifs").prepend(gifDiv);
-            }
-        });
+    //             $("#displayGifs").prepend(gifDiv);
+    //         }
+    //     });
 });
